476f0aac50939d755162575856ae8493b5a63bf6
Hariom Kuamr <hariompandit5556@gmail.com>
2024-09-25 11:13:28 +0530

    lib

--- src/lib/generateRssFeed.js
+++ src/lib/generateRssFeed.js
@@ -8,8 +8,8 @@ export async function generateRssFeed() {
   let articles = await getAllArticles()
   let siteUrl = process.env.NEXT_PUBLIC_SITE_URL
   let author = {
-    name: 'Victoria Jordan',
-    email: 'vjordan.cs@gmail.com',
+    name: 'Hariom Kumar',
+    email: 'hariompandit5556@gmail.com',
   }
 
   let feed = new Feed({
