fe0e82e857ceea04fe95dd742fac0ff231402fe4
HARIOM KUMAR PANDIT <138400713+hari7261@users.noreply.github.com>
2024-10-03 18:29:15 +0530

    resume updated

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -177,7 +177,7 @@ function Resume() {
           </li>
         ))}
       </ol>
-      <Button href="https://drive.google.com/file/d/1CqlXLizHphzIvT4cXOx-PqZUViz_FdrC/view?usp=sharing" variant="secondary" className="group mt-6 w-full">
+      <Button href="https://drive.google.com/file/d/1NvCkZ0ruM_yIEi213kJ81JeIgPAltqXB/view?usp=sharing" variant="secondary" className="group mt-6 w-full">
         Download CV
         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
       </Button>
