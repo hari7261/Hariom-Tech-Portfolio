2428e3127e1a25c96c67993fc0850602991f114d
Hariom Kuamr <hariompandit5556@gmail.com>
2024-11-06 23:57:29 +0530

    img

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -41,20 +41,20 @@ export default function Document() {
       <Head>
         <link rel="icon" href="https://avatars.githubusercontent.com/u/138400713?v=4" />
         <meta name="description" content="I'm Hariom, a full stack developer/software engineer exploring the world of software and AI." />
-        
+
         {/* Open Graph Meta Tags */}
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://hariom-react-next.vercel.app/home" />
+        <meta property="og:url" content="https://hariompandit.me" />
         <meta property="og:title" content="Hariom Kumar - Full Stack Developer/Software Engineer" />
         <meta property="og:description" content="I'm Hariom, a full stack developer/software engineer exploring the world of software and AI." />
-        <meta property="og:image" content="https://pub-0cd6f9d4131f4f79ac40219248ae64db.r2.dev/Screenshot%202024-07-19%20at%202.36.33%E2%80%AFAM.png" />
+        <meta property="og:image" content="/src/images/photos/image.png" />
 
         {/* Twitter Card Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:url" content="https://hariom-react-next.vercel.app/home" />
+        <meta name="twitter:url" content="https://hariompandit.me" />
         <meta name="twitter:title" content="Hariom Kumar - Full Stack Developer/Software Engineer" />
         <meta name="twitter:description" content="I'm Hariom, a full stack developer/software engineer exploring the world of software and AI." />
-        <meta name="twitter:image" content="https://pub-0cd6f9d4131f4f79ac40219248ae64db.r2.dev/Screenshot%202024-07-19%20at%202.36.33%E2%80%AFAM.png" />
+        <meta name="twitter:image" content="/src/images/photos/image.png" />
 
         <script dangerouslySetInnerHTML={{ __html: modeScript }} />
         <link rel="alternate" type="application/rss+xml" href={`${process.env.NEXT_PUBLIC_SITE_URL}/rss/feed.xml`} />
